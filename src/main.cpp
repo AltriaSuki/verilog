11022f2d6e27eaa2b7c0c423872d2c41036eebde
AltriaSuki <1341951274@qq.com>
2025-05-23 10:55:54 +0800

    修改了词法分析，新添加了~运算符

--- src/main.cpp
+++ src/main.cpp
@@ -6,27 +6,27 @@
 #include"ILP.h"
 #include"Node.h"
 int main(){
-    // Lexer lexer("module test(a,b,c) wire e; assign a=b*c+d;assign e=b*c+f; endmodule");
-    // Parser parser(lexer);
-    // AstNode* node=parser.parseProgram();
+    Lexer lexer("module test(a,b,c) wire e; assign a=~b; endmodule");
+    Parser parser(lexer);
+    AstNode* node=parser.parseProgram();
     // Optimization opt=Optimization(node);
     // opt.optimize_in_Ast();
-    // node->print(0);
-    // RTLILGen rtlil(node);
-    // std::string result=rtlil.generateRTLIL();
+    node->print(0);
+    RTLILGen rtlil(node);
+    std::string result=rtlil.generateRTLIL();
     // opt.set_rtlil_code(result);
     // opt.optimize_in_rtlil();
     // result=opt.get_optimized_rtlil();
-    // std::cout<<result<<std::endl;
-    ILP ilp;
-    std::string filename="./input/sample.blif";
-    ilp.set_delay_map({{"AND",1},{"OR",1},{"NOT",1}});
-    ilp.read_blif(filename);
-    std::map<std::string,int> constraints;
-    constraints["AND"]=2;
-    constraints["OR"]=1;
-    constraints["NOT"]=2;
-    ilp.make_constraints(constraints);
+    std::cout<<result<<std::endl;
+    // ILP ilp;
+    // std::string filename="./input/sample.blif";
+    // ilp.set_delay_map({{"AND",1},{"OR",1},{"NOT",1}});
+    // ilp.read_blif(filename);
+    // std::map<std::string,int> constraints;
+    // constraints["AND"]=2;
+    // constraints["OR"]=1;
+    // constraints["NOT"]=2;
+    // ilp.make_constraints(constraints);
     return 0;
 }
 
